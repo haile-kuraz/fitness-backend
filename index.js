0d23ed810f3ebf8047c7542232bb70d48b7fc00d
haile <haile@kali.kali>
2023-07-23 06:58:49 -0400

    removing netlify

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Serverless = require("serverless-http");
 
-const fittModel = require("../model/FittModel");
+const fittModel = require("./model/FittModel");
 
 const app = express();
 const router = express.Router();
